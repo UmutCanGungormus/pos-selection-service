933f03b03f68cccb43ce80237c31ffa65e21dcf7
Umut Can Gungormus <umutcangungormus@gmail.com>
2026-03-31 10:40:26 +0300

    Add check for Composer dependencies in entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+if [ ! -f /var/www/vendor/autoload.php ]; then
+    composer install --no-interaction --prefer-dist
+fi
+
 php artisan config:cache
 php artisan route:cache
 php artisan event:cache
